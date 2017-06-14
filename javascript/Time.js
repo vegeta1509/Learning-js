b6cbf6d189d3e83a798c8ea175f0fb029597f930
vegeta1509 <ayushmvi123@gmail.com>
2017-06-15 01:13:29 +0530

    starting

---
+++ javascript/Time.js
@@ -0,0 +1,20 @@
+var time=3;
+console.log('hi vegeta '+'hi'); 
+/*setTimeout(function(){
+    console.log('3 seconds have passes');
+},3000)*/
+
+/*setInterval(
+    function(){
+        time += 2;
+        console.log(time+' seconds passed');
+    },3000
+)*/
+/*var timer=setInterval(
+    function(){
+        time += 2;
+        console.log(time+' seconds passed');
+        if(time>7){clearInterval(timer);}
+    },3000
+)*/
+console.log(__filename);
